f612481f10b46da20e63e989360a1318557587e2
fa11ingsky <daniel.papadopoulos94@gmail.com>
2022-10-10 18:17:21 +1100

    Added dynamic product loading from inventory JSON

--- assets/js/productinfo.js
+++ assets/js/productinfo.js
@@ -1,13 +1,34 @@
 // Product view
 // fetch inventory
 $.getJSON("assets/inventory.json", (inventory) => {
-    for (let product in inventory) {
-        $(`.${inventory[product].cart}`).on("click", () => {
-            // fetch cart
-            let cart = JSON.parse(sessionStorage.getItem("cart"));
-            cart[product] = product in cart ? cart[product] + 1 : 1;
-            // store cart
-            sessionStorage.setItem("cart", JSON.stringify(cart));
-        });
+    var productRowStart = `<div class="row product-row">`;
+    var productRowEnd = `</div>`;
+    var entries = 1;
+    var productPage = productRowStart;
+    for (let product in inventory) {   
+        if (entries % 3 == 0) {
+            productage += productRowEnd + productRowStart;
+        }
+            productPage += `<div class="col-md-4  text-center pokemon">
+                <div class="single-product-item">
+                    <div class="product-image">
+                        <a><img src="assets/img/products/${inventory[product].img}" alt=""></a>
+                    </div>
+                    <h3>${product}</h3>
+                    <p class="product-price"> $${inventory[product].price} </p>
+                    <a class=".${inventory[product].cart} cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+                </div>
+            </div>`;        
+
+            $(`.${inventory[product].cart}`).on("click", () => {
+                // fetch cart
+                let cart = JSON.parse(sessionStorage.getItem("cart"));
+                cart[product] = product in cart ? cart[product] + 1 : 1;
+                // store cart
+                sessionStorage.setItem("cart", JSON.stringify(cart));
+            });
+
     }
+    productPage += productRowEnd;
+    $(".products").html(productPage);
 });

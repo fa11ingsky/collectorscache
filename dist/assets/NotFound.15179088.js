1d5e62522038f73b42d204b5cf18c81fdf4d0497
fa11ingsky <daniel.papadopoulos94@gmail.com>
2022-10-20 21:59:29 +1100

    Deployment

---
+++ dist/assets/NotFound.15179088.js
@@ -0,0 +1,1 @@
+import{_ as o,c as e,o as n}from"./index.22ba9ebe.js";const t={},c={class:"notfound"};function s(r,_){return n(),e("div",c," Page not found ")}const d=o(t,[["render",s]]);export{d as default};

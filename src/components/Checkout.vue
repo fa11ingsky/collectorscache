0865a310fe9290fbd9e0db8921ba0d79b642d11d
fa11ingsky <daniel.papadopoulos94@gmail.com>
2022-10-23 22:46:55 +1100

    promo json and field added to checkout

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -30,10 +30,15 @@
         <hr>
         <div class="shipping-est row">
             <div class="deliver-to col-sm-3">Ship to:</div>
-            <select v-model="country"  class="col form-select">
+            <select v-model="country" class="col form-select">
                 <option>Australia</option>
                 <option>International</option>
             </select>
+        </div>
+        <hr>
+        <!-- Promo code -->
+        <div class="shipping-est row">
+            <input class="form-control" v-model="promoCode" placeholder="Promo Code"/>
         </div>
         <hr>
         <div class="checkout">
@@ -44,6 +49,10 @@
             <div class="row">
                 <div class="total-items items col">{{this.$root.cartItems}} items</div>
             </div>
+            <div class="row" v-if="promoApplied">
+                <div class="col">Discount</div>
+                <div class="cost col">{{discount}}% ({{promoCode}})</div>
+            </div>
             <div class="row">
                 <div class="col">Shipping</div>
                 <div class="cost col">${{shipping}}</div>
@@ -51,7 +60,7 @@
             <br />
             <div class="row">
                 <div class="total col">Total</div>
-                <div class="total cost col">${{subtotal + shipping}}</div>
+                <div class="total cost col">AUD ${{discountTotal + shipping}}</div>
             </div>
         </div>
         <hr>
@@ -65,17 +74,37 @@
     </div>
 </template>
 <script>
+    import promos from "../assets/promos.json"
+
     export default {
         data() {
             return {
                 subtotal: 0,
-                description: "",
-                country: "Australia"
+                country: "Australia",
+                promoCode: ""
             }
         },
         computed: {
             shipping() {
                 return this.country == "Australia" ? 10 : 20
+            },
+            promoApplied() {
+                return this.promoCode.trim() in promos
+            },
+            discount() {
+                return promos[this.promoCode.trim()] || 0
+            },
+            discountTotal() {
+                return parseFloat((this.subtotal*(1-(this.discount/100))).toFixed(2))
+            },
+            description() {
+                let descr = ""
+                for (let product in this.$root.cart) {
+                    descr += this.$root.cart[product] > 0 ? `${product} x${this.$root.cart[product]} ; ` : ''
+                }
+                descr += this.promoCode.trim() in promos ? `${this.promoCode.trim()}; ` : ''
+                descr += `shp${this.shipping}; `
+                return descr
             }
         },
         methods: {
@@ -108,7 +137,7 @@
 
                     createOrder: function (data, actions) {
                         return actions.order.create({
-                            purchase_units: [{ "description": self.description, "amount": { "currency_code": "AUD", "value": self.subtotal + self.shipping } }]
+                            purchase_units: [{ "description": self.description, "amount": { "currency_code": "AUD", "value": self.discountTotal + self.shipping } }]
                         });
                     },
 
@@ -133,17 +162,15 @@
                 }).render('#paypal-button-container');
             },
             // Pull the cart data from localstorage and re-calculate total items and description for paypal checkout
+            // TOOD: make this obsolete with computed variables
             refreshCart: function () {
                 this.$root.cart = localStorage.cart ? JSON.parse(localStorage.cart) : {}
                 this.subtotal = 0
                 this.$root.cartItems = 0
-                this.description = ""
                 for (let product in this.$root.cart) {
                     this.subtotal += this.$root.inventory[product].price * this.$root.cart[product];
                     this.$root.cartItems += this.$root.cart[product];
-                    this.description += this.$root.cart[product] > 0 ? `${product} x${this.$root.cart[product]} ; ` : ''
                 }
-                this.description += `shp${this.shipping} ; `
             },
             btoa: function (s) {
                 return btoa(s)
